df74c5336106fd7d2eb3e87684f30c488a6bad45
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2017-11-19 20:57:04 -0800

    fix

--- app.js
+++ app.js
@@ -48,8 +48,10 @@ app.use(function(err, req, res, next) {
   res.render('500');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Express started on http://localhost:' +
-    app.get('port') +
-    '; press Ctrl-C to terminate.');
-});
+// app.listen(app.get('port'), function() {
+//   // console.log('Express started on http://localhost:' +
+//   //   app.get('port') +
+//   //   '; press Ctrl-C to terminate.');
+// });
+
+app.listen(process.env.PORT || 8080)
